8b5d32b5f824fba895e4d37de5b5353f7498f649
yuwei91 <851968421@qq.com>
2020-06-28 18:31:05 +0800

    完成index和部分user

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,7 +7,12 @@ $.ajaxPrefilter(function (options) {
   // options.url = 'http://127.0.0.1:3700' + options.url
 
   // 统一为有权限的接口，设置 headers 请求头
-  if (options.url.indexOf('/my/') !== -1) {
+  // if (options.url.indexOf('/my/') !== -1) {
+  //   options.headers = {
+  //     Authorization: localStorage.getItem('token') || '',
+  //   }
+  // }
+  if (options.url.includes('/my/')) {
     options.headers = {
       Authorization: localStorage.getItem('token') || '',
     }
@@ -16,13 +21,13 @@ $.ajaxPrefilter(function (options) {
   // 全局统一挂载 complete 回调函数
   options.complete = function (res) {
     // console.log('执行了 complete 回调：')
-    // console.log(res)
+    const { status, message } = res.responseJSON
     // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+    if (status === 1 && message === '身份认证失败！') {
       // 1. 强制清空 token
       localStorage.removeItem('token')
       // 2. 强制跳转到登录页面
-      location.href = '/login.html'
+      location.href = '/my01/bigevent/login.html'
     }
   }
 })
