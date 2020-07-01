3b1fc6a6237c2e63531192bbedd094faeabcbae6
yuwei91 <851968421@qq.com>
2020-07-01 21:20:25 +0800

    完成文章管理

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,10 +1,11 @@
-$(function() {
+$(function () {
+
   var layer = layui.layer
   var form = layui.form
   var laypage = layui.laypage
 
   // 定义美化时间的过滤器
-  template.defaults.imports.dataFormat = function(date) {
+  template.defaults.imports.dataFormat = function (date) {
     const dt = new Date(date)
 
     var y = dt.getFullYear()
@@ -15,13 +16,14 @@ $(function() {
     var mm = padZero(dt.getMinutes())
     var ss = padZero(dt.getSeconds())
 
-    return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
   }
 
   // 定义补零的函数
   function padZero(n) {
     return n > 9 ? n : '0' + n
   }
+
 
   // 定义一个查询的参数对象，将来请求数据的时候，
   // 需要将请求参数对象提交到服务器
@@ -41,7 +43,7 @@ $(function() {
       method: 'GET',
       url: '/my/article/list',
       data: q,
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('获取文章列表失败！')
         }
@@ -59,7 +61,8 @@ $(function() {
     $.ajax({
       method: 'GET',
       url: '/my/article/cates',
-      success: function(res) {
+      success: function (res) {
+        console.log(res)
         if (res.status !== 0) {
           return layer.msg('获取分类数据失败！')
         }
@@ -73,7 +76,7 @@ $(function() {
   }
 
   // 为筛选表单绑定 submit 事件
-  $('#form-search').on('submit', function(e) {
+  $('#searchform').on('submit', function (e) {
     e.preventDefault()
     // 获取表单中选中项的值
     var cate_id = $('[name=cate_id]').val()
@@ -85,33 +88,19 @@ $(function() {
     initTable()
   })
 
-  // 定义渲染分页的方法
+  //渲染分页的函数
   function renderPage(total) {
-    // 调用 laypage.render() 方法来渲染分页的结构
     laypage.render({
-      elem: 'pageBox', // 分页容器的 Id
-      count: total, // 总数据条数
-      limit: q.pagesize, // 每页显示几条数据
-      curr: q.pagenum, // 设置默认被选中的分页
-      layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+      elem: 'pagebox',
+      count: total,
+      limit: q.pagesize,
+      curr: q.pagenum,
       limits: [2, 3, 5, 10],
-      // 分页发生切换的时候，触发 jump 回调
-      // 触发 jump 回调的方式有两种：
-      // 1. 点击页码的时候，会触发 jump 回调
-      // 2. 只要调用了 laypage.render() 方法，就会触发 jump 回调
-      jump: function(obj, first) {
-        // 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
-        // 如果 first 的值为 true，证明是方式2触发的
-        // 否则就是方式1触发的
-        console.log(first)
-        console.log(obj.curr)
-        // 把最新的页码值，赋值到 q 这个查询参数对象中
-        q.pagenum = obj.curr
-        // 把最新的条目数，赋值到 q 这个查询参数对象的 pagesize 属性中
-        q.pagesize = obj.limit
-        // 根据最新的 q 获取对应的数据列表，并渲染表格
-        // initTable()
-        if (!first) {
+      layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+      jump: function (object, isfirst) {
+        q.pagenum = object.curr
+        q.pagesize = object.limit
+        if (!isfirst) {
           initTable()
         }
       }
@@ -120,15 +109,14 @@ $(function() {
 
   // 通过代理的形式，为删除按钮绑定点击事件处理函数
   $('tbody').on('click', '.btn-delete', function() {
-    // 获取删除按钮的个数
+    // 获取删除按钮的个数  (当前页面的删除按钮个数)
     var len = $('.btn-delete').length
-    console.log(len)
-    // 获取到文章的 id
-    var id = $(this).attr('data-id')
+    // 获取到文章的 id  
+    var id = $(this).data('id')
     // 询问用户是否要删除数据
     layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
       $.ajax({
-        method: 'GET',
+        type: 'get',
         url: '/my/article/delete/' + id,
         success: function(res) {
           if (res.status !== 0) {
@@ -147,8 +135,11 @@ $(function() {
           initTable()
         }
       })
-
       layer.close(index)
     })
   })
+
 })
+
+
+
